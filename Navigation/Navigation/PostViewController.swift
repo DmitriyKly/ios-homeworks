78ca6c0d5fd00f57997b2382c61782fbf7e61cde
Dmitriy <dmitriykljuev@gmail.com>
2023-06-01 17:06:27 +0300

    FirstCommit

---
+++ Navigation/Navigation/PostViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PostViewController.swift
+//  Navigation
+//
+//  Created by dmitriy on 1/6/2023.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        title = "Post"
+    }
+}
