c115655b18c6ce423b9a322980e6bc5c6110bbfa
Dmitriy <dmitriykljuev@gmail.com>
2023-07-01 11:16:50 +0300

    fixed open Image

--- Navigation/Navigation/Profile/ProfileTableHeaderView.swift
+++ Navigation/Navigation/Profile/ProfileTableHeaderView.swift
@@ -6,67 +6,9 @@
 //
 
 import UIKit
+class ProfileHeaderView: UIView {
+    
 
-class ProfileHeaderView: UIView, UITextFieldDelegate {
-    
-    var isImageExpanded = false
-    
-    lazy var avatarImageView: UIView = {
-        let avatarImageView = UIView()
-        avatarImageView.layer.cornerRadius = 60
-        avatarImageView.translatesAutoresizingMaskIntoConstraints = false
-        avatarImageView.backgroundColor = .white
-        avatarImageView.layer.borderColor = UIColor.white.cgColor
-        avatarImageView.layer.borderWidth = 3
-        
-        return avatarImageView
-    }()
-    
-    lazy var fullNameLabel: UILabel = {
-        let fullNameLabel = UILabel()
-        fullNameLabel.text = "Snoop Dog"
-        fullNameLabel.font = UIFont.boldSystemFont(ofSize: 18.0)
-        fullNameLabel.textColor = UIColor.black
-        fullNameLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        return fullNameLabel
-    }()
-    
-    lazy var imageViewSnoopDog: UIImageView = {
-        let imageViewSnoopDog = UIImageView()
-        imageViewSnoopDog.image = UIImage(named: "Snoop dog")
-        imageViewSnoopDog.contentMode = .scaleAspectFill
-        imageViewSnoopDog.clipsToBounds = true
-        imageViewSnoopDog.translatesAutoresizingMaskIntoConstraints = false
-        imageViewSnoopDog.layer.cornerRadius = avatarImageView.layer.cornerRadius
-        imageViewSnoopDog.layer.borderColor = avatarImageView.layer.borderColor
-        imageViewSnoopDog.layer.borderWidth = avatarImageView.layer.borderWidth
-        imageViewSnoopDog.isUserInteractionEnabled = true
-        
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleImageTap))
-        tapGesture.numberOfTapsRequired = 1
-        imageViewSnoopDog.addGestureRecognizer(tapGesture)
-        
-        return imageViewSnoopDog
-    }()
-    
-    lazy var overlayView: UIView = {
-        let overlayView = UIView(frame: bounds)
-        overlayView.backgroundColor = UIColor.black.withAlphaComponent(0.8)
-        overlayView.alpha = 0
-        overlayView.isUserInteractionEnabled = false
-        return overlayView
-    }()
-    
-    lazy var closeButton: UIButton = {
-        let closeButton = UIButton(type: .custom)
-        closeButton.setImage(UIImage(named: "close_icon"), for: .normal)
-        closeButton.alpha = 0
-        closeButton.addTarget(self, action: #selector(handleCloseButtonTap), for: .touchUpInside)
-        closeButton.frame = CGRect(x: frame.width - 40, y: 20, width: 30, height: 30)
-        return closeButton
-    }()
-    
     lazy var statusLabel: UILabel = {
         let statusLabel = UILabel()
         statusLabel.text = "Write Your Status"
@@ -75,21 +17,6 @@ class ProfileHeaderView: UIView, UITextFieldDelegate {
         statusLabel.translatesAutoresizingMaskIntoConstraints = false
         
         return statusLabel
-    }()
-    
-    lazy var statusTextField: UITextField = {
-        let statusTextField = UITextField()
-        statusTextField.backgroundColor = UIColor.white
-        statusTextField.layer.cornerRadius = 12
-        statusTextField.textColor = UIColor.black
-        statusTextField.layer.borderWidth = 1
-        statusTextField.layer.borderColor = UIColor.black.cgColor
-        statusTextField.font = UIFont.systemFont(ofSize: 15, weight: .regular)
-        statusTextField.translatesAutoresizingMaskIntoConstraints = false
-        statusTextField.delegate = self
-        statusTextField.addTarget(nil, action: Selector(("firstResponderAction:")), for: .editingDidEndOnExit)
-        statusTextField.placeholder = "Set your status.."
-        return statusTextField
     }()
     
     lazy var setStatusButton: UIButton = {
@@ -106,100 +33,165 @@ class ProfileHeaderView: UIView, UITextFieldDelegate {
         return setStatusButton
     }()
     
+    private let avatarWrapView: UIView = {
+        let avatarWrapView = UIView()
+        avatarWrapView.translatesAutoresizingMaskIntoConstraints = false
+        return avatarWrapView
+    }()
+    
+    private let blackBackground: UIView = {
+        let blackBackground = UIView()
+        blackBackground.translatesAutoresizingMaskIntoConstraints = false
+        blackBackground.backgroundColor = .black
+        blackBackground.layer.opacity = 0.5
+        blackBackground.isHidden = true
+        return blackBackground
+    }()
+    
+    private lazy var imageViewSnoopDog: UIImageView = {
+        let imageViewSnoopDog = UIImageView()
+          imageViewSnoopDog.image = UIImage(named: "Snoop dog")
+          imageViewSnoopDog.contentMode = .scaleAspectFill
+          imageViewSnoopDog.clipsToBounds = true
+          imageViewSnoopDog.translatesAutoresizingMaskIntoConstraints = false
+          imageViewSnoopDog.layer.cornerRadius = 50
+          imageViewSnoopDog.layer.borderColor = UIColor.white.cgColor
+          imageViewSnoopDog.layer.borderWidth = 3
+          imageViewSnoopDog.isUserInteractionEnabled = true
+          imageViewSnoopDog.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapAction)))
+  
+          return imageViewSnoopDog
+    }()
+    
+    private lazy var closeAvatar: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.image =  UIImage(systemName: "xmark.circle")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.isUserInteractionEnabled = true
+        imageView.layer.opacity = 0
+        imageView.tintColor = .white
+        imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapCloseGesture)))
+        return imageView
+    }()
+    
+    lazy var fullNameLabel: UILabel = {
+        let fullNameLabel = UILabel()
+        fullNameLabel.text = "Snoop Dog"
+        fullNameLabel.font = UIFont.boldSystemFont(ofSize: 18.0)
+        fullNameLabel.textColor = UIColor.black
+        fullNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        return fullNameLabel
+    }()
+
+    lazy var statusTextField: UITextField = {
+        let statusTextField = UITextField()
+        statusTextField.backgroundColor = UIColor.white
+        statusTextField.layer.cornerRadius = 12
+        statusTextField.textColor = UIColor.black
+        statusTextField.layer.borderWidth = 1
+        statusTextField.layer.borderColor = UIColor.black.cgColor
+        statusTextField.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        statusTextField.translatesAutoresizingMaskIntoConstraints = false
+        statusTextField.addTarget(nil, action: Selector(("firstResponderAction:")), for: .editingDidEndOnExit)
+        statusTextField.placeholder = "Set your status.."
+        return statusTextField
+    }()
+    
+    private var leadingImageView = NSLayoutConstraint()
+    private var trailingImageView = NSLayoutConstraint()
+    private var topImageView = NSLayoutConstraint()
+    private var widthImageView = NSLayoutConstraint()
+    private var heightImageView = NSLayoutConstraint()
+    private var heightWrapView = NSLayoutConstraint()
+    
+    //MARK: - inits
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addButtons()
+        addSubviews()
+        setupContraints()
+    }
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupView()
-        setupContraints()
-        addButtons()
+    //MARK: - Functions
+    
+    private func addSubviews(){
+        addSubview(fullNameLabel)
+        addSubview(statusLabel)
+        addSubview(setStatusButton)
+        addSubview(statusTextField)
+        addSubview(avatarWrapView)
+        addSubview(blackBackground)
+        addSubview(imageViewSnoopDog)
+        addSubview(closeAvatar)
     }
     
-    func setupView() {
-        
-        self.addSubview(avatarImageView)
-        self.addSubview(fullNameLabel)
-        self.addSubview(statusLabel)
-        self.addSubview(setStatusButton)
-        self.addSubview(imageViewSnoopDog)
-        self.addSubview(statusTextField)
-        self.addSubview(overlayView)
-        self.addSubview(closeButton)
-    }
-    
-    func addButtons() {
+    private func addButtons() {
         setStatusButton.addTarget(self, action: #selector(printStatus), for: .touchUpInside)
-    }
-    
-    @objc func handleImageTap() {
-        if isImageExpanded {
-            UIView.animate(withDuration: 0.5) {
-                self.imageViewSnoopDog.transform = .identity
-                self.overlayView.alpha = 0
-            }
-            UIView.animate(withDuration: 0.3, delay: 0.2, options: [])
-            {
-                self.closeButton.alpha = 0
-            }
-        } else {
-            UIView.animate(withDuration: 0.5) {
-                let scale = UIScreen.main.bounds.width / self.imageViewSnoopDog.frame.width
-                self.imageViewSnoopDog.transform = CGAffineTransform(scaleX: scale, y: scale)
-                self.imageViewSnoopDog.center = self.center
-                self.overlayView.alpha = 1
-            }
-            UIView.animate(withDuration: 0.3, delay: 0.5, options: []){
-                self.closeButton.alpha = 1
-            }
-        }
         
-        isImageExpanded = !isImageExpanded
     }
     
-    @objc func handleCloseButtonTap() {
-        UIView.animate(withDuration: 0.3) {
-            self.closeButton.alpha = 0
-        }
-        UIView.animate(withDuration: 0.5, delay: 0.3, options: []) {
-            self.imageViewSnoopDog.transform = .identity
-            self.overlayView.alpha = 0
-        }
-        isImageExpanded = false
-    }
+    //MARK: - Constraints
     
-    func setupContraints() {
+    private func setupContraints() {
+        
+        //                  imageViewSnoopDog
+        leadingImageView = imageViewSnoopDog.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16)
+        topImageView = imageViewSnoopDog.topAnchor.constraint(equalTo: topAnchor,constant: 16)
+        widthImageView = imageViewSnoopDog.widthAnchor.constraint(equalToConstant: 95)
+        heightImageView = imageViewSnoopDog.heightAnchor.constraint(equalToConstant: 95)
+        NSLayoutConstraint.activate([leadingImageView, topImageView, widthImageView, heightImageView])
+        
+        //                  wrapView
+        heightWrapView = blackBackground.heightAnchor.constraint(equalToConstant: 0)
+        NSLayoutConstraint.activate([heightWrapView])
         
         NSLayoutConstraint.activate([
             
-            avatarImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 16),
-            avatarImageView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
             
-            imageViewSnoopDog.topAnchor.constraint(equalTo: avatarImageView.topAnchor,constant: 0),
-            imageViewSnoopDog.leadingAnchor.constraint(equalTo: avatarImageView.leadingAnchor,constant: 0),
-            imageViewSnoopDog.trailingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 0),
-            imageViewSnoopDog.bottomAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 0),
-            imageViewSnoopDog.widthAnchor.constraint(equalToConstant: 120),
-            imageViewSnoopDog.heightAnchor.constraint(equalToConstant: 120),
             
-            fullNameLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 27),
-            fullNameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 27),
-            fullNameLabel.widthAnchor.constraint(equalTo: widthAnchor),
+            blackBackground.leadingAnchor.constraint(equalTo: leadingAnchor),
+            blackBackground.trailingAnchor.constraint(equalTo: trailingAnchor),
+            blackBackground.topAnchor.constraint(equalTo: topAnchor),
             
-            statusLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor,constant: 27),
-            statusLabel.bottomAnchor.constraint(equalTo: statusTextField.topAnchor, constant: -16),
+            closeAvatar.widthAnchor.constraint(equalToConstant: 30),
+            closeAvatar.heightAnchor.constraint(equalToConstant: 30),
+            closeAvatar.topAnchor.constraint(equalTo: topAnchor, constant: 5),
+            closeAvatar.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -5),
             
-            statusTextField.leadingAnchor.constraint(equalTo: statusLabel.leadingAnchor),
-            statusTextField.trailingAnchor.constraint(equalTo: setStatusButton.trailingAnchor),
-            statusTextField.bottomAnchor.constraint(equalTo: setStatusButton.topAnchor, constant: -16),
-            statusTextField.heightAnchor.constraint(equalToConstant: 40),
+            avatarWrapView.widthAnchor.constraint(equalToConstant: 95),
+            avatarWrapView.heightAnchor.constraint(equalToConstant: 95),
+            avatarWrapView.topAnchor.constraint(equalTo: topAnchor,constant: 16),
+            avatarWrapView.leadingAnchor.constraint(equalTo: leadingAnchor,constant: 16),
             
-            setStatusButton.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 16),
-            setStatusButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            setStatusButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            fullNameLabel.widthAnchor.constraint(equalToConstant: 100),
+            fullNameLabel.topAnchor.constraint(equalTo: avatarWrapView.topAnchor ,constant: 0),
+            fullNameLabel.leadingAnchor.constraint(equalTo: avatarWrapView.trailingAnchor,constant: 20),
+            fullNameLabel.trailingAnchor.constraint(equalTo: trailingAnchor,constant: -16),
+            
+            statusLabel.topAnchor.constraint(equalTo: fullNameLabel.bottomAnchor,constant: 10),
+            statusLabel.leadingAnchor.constraint(equalTo: fullNameLabel.leadingAnchor),
+            statusLabel.trailingAnchor.constraint(equalTo: fullNameLabel.trailingAnchor),
+            
+            statusTextField.widthAnchor.constraint(equalTo: statusLabel.widthAnchor),
+            statusTextField.heightAnchor.constraint(equalToConstant: 50),
+            statusTextField.topAnchor.constraint(equalTo: statusLabel.bottomAnchor,constant: 15),
+            statusTextField.leadingAnchor.constraint(equalTo: fullNameLabel.leadingAnchor),
+            statusTextField.trailingAnchor.constraint(equalTo: fullNameLabel.trailingAnchor),
+            
             setStatusButton.heightAnchor.constraint(equalToConstant: 50),
-            setStatusButton.bottomAnchor.constraint(equalTo: bottomAnchor)
+            setStatusButton.topAnchor.constraint(equalTo: statusTextField.bottomAnchor, constant: 15),
+            setStatusButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -15),
+            setStatusButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            setStatusButton.trailingAnchor.constraint(equalTo: statusTextField.trailingAnchor)
         ])
     }
     
@@ -207,5 +199,50 @@ class ProfileHeaderView: UIView, UITextFieldDelegate {
         statusLabel.text = statusTextField.text
         statusTextField.text = ""
     }
-}
+    
+    @objc private func tapAction() {
+        UIView.animateKeyframes(withDuration: 0.8, delay: 0) {
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.6) {
+                
+                self.widthImageView.constant = UIScreen.main.bounds.width - 60
+                self.leadingImageView.constant = 30
+                self.trailingImageView.constant = 30
+                self.topImageView.constant = 30
+                self.heightImageView.constant = UIScreen.main.bounds.height - 220
+                self.heightWrapView.constant =  UIScreen.main.bounds.height
+                self.imageViewSnoopDog.layer.cornerRadius = 0
+                self.blackBackground.isHidden = false
+            }
+            UIView.addKeyframe(withRelativeStartTime: 0.6, relativeDuration: 0.8) {
+                self.closeAvatar.layer.opacity = 1
+                self.blackBackground.layer.opacity = 0.3
 
+            }
+        }
+        
+    }
+    
+    @objc private func tapCloseGesture() {
+        
+        UIView.animateKeyframes(withDuration: 0.4, delay: 0) {
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.5) {
+                self.closeAvatar.layer.opacity = 0
+                self.blackBackground.layer.opacity = 0
+            }
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.5) {
+                self.blackBackground.isHidden = false
+                self.blackBackground.isHidden = true
+                self.imageViewSnoopDog.layer.cornerRadius = 50
+                self.widthImageView.constant = 95
+                self.leadingImageView.constant = 16
+                self.trailingImageView.constant = 30
+                self.topImageView.constant = 16
+                self.heightImageView.constant = 95
+                self.heightWrapView.constant =  UIScreen.main.bounds.height
+            }
+        }
+    }
+}
